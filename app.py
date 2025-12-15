c2ec074e68f1180a7cc934b357d4d9f59b42f5c9
Omar Mezzour <omar_mez@hotmail.fr>
2025-12-15 08:10:49 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+#importing required libraries
+
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+from sklearn import metrics
+import warnings
+import pickle
+from convert import convertion
+warnings.filterwarnings('ignore')
+from feature import FeatureExtraction
+
+file = open("newmodel.pkl","rb")
+gbc = pickle.load(file)
+file.close()
+
+
+app = Flask(__name__)
+#from flask import Flask, render_template, request
+@app.route("/")
+def home():
+    return render_template("index.html")
+@app.route('/result',methods=['POST','GET'])
+def predict():
+    if request.method == "POST":
+        url = request.form["name"]
+        obj = FeatureExtraction(url)
+        x = np.array(obj.getFeaturesList()).reshape(1,30)
+    
+        y_pred =gbc.predict(x)[0]
+  
+        name=convertion(url,int(y_pred))
+        return render_template("index.html", name=name)
+
+if __name__ == "__main__":
+    app.run(debug=True)
